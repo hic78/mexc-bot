751249f2435d87028aecb0975e114fdebf1a5423
hic78 <h.fourali2@gmail.com>
2026-06-14 16:05:01 +0200

    auto-backup: 2026-06-14 16:05

---
+++ uni.py
@@ -0,0 +1,6 @@
+import xsmom2_bot as b, time
+c=b.MEXCRestClient()
+sym=b.to_mexc_symbol("UNI")
+# historique des deals/ordres remplis sur UNI
+print("=== ordres UNI dans le log bot ===")
+import subprocess
